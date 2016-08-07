d79a7aefd7fdee8b8f0761e7164b2618a9eea906
Dave Lively <davelively14@gmail.com>
2016-08-07 08:47:40 -0400

    Added handling for adding a member to a board

--- web/channels/board_channel.ex
+++ web/channels/board_channel.ex
@@ -1,6 +1,6 @@
 defmodule PhoenixTrello.BoardChannel do
   use PhoenixTrello.Web, :channel
-  alias PhoenixTrello.Board
+  alias PhoenixTrello.{UserBoard, User}
 
   def join("boards:" <> board_id, _params, socket) do
     board = get_current_board(socket, board_id)
@@ -8,8 +8,40 @@ defmodule PhoenixTrello.BoardChannel do
     {:ok, %{board: board}, assign(socket, :board, board)}
   end
 
+  def handle_in("members:add", %{"email" => email}, socket) do
+    try do
+      board = socket.assigns.board
+      user =
+        User
+        |> Repo.get_by(email)
+
+      changeset =
+        user
+        |> build_assoc(:user_boards)
+        |> UserBoard.changeset(%{board_id: board.id})
+
+      case Repo.insert(changeset) do
+        {:ok, _board_user} ->
+          broadcast! socket, "member:added", %{user: user}
+
+          # TODO you can broadcast to any channel from any other channel
+          # Broadcasts the message "boards:add" to the channel of the user
+          # who was just given access to the board so that he can begin using it
+          # right away.
+          PhoenixTrello.Endpoint.broadcast_from! self(), "users:#{user.id}", "boards:add", %{board: board}
+
+          {:noreply, socket}
+        {:error, _changeset} ->
+          {:reply, {:error, %{error: "Error adding new member"}}, socket}
+      end
+    catch
+      _, _ ->
+        {:reply, {:error, %{error: "User does not exist"}}, socket}
+    end
+  end
+
   # I don't understand why we're doing an association on a Repo.get? Should just
-  # be: Repo.get(Board, board_id). 
+  # be: Repo.get(Board, board_id).
   defp get_current_board(socket, board_id) do
     socket.assigns.current_user
     |> assoc(:boards)
